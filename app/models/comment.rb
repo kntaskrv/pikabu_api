59ac14b33cf3de38e50bdde6f61359ca67bd1128
kntaskrv <kntaskrv@gmail.com>
2020-07-03 11:34:39 +0600

    Added pagination
    instaled gem 'pagy'
    created actions for get posts and comments
    added scopes to post and comments for sorting and filtration

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,4 +6,7 @@ class Comment < ApplicationRecord
 
   has_many :images, as: :imageable
   has_many :rates, as: :rateable
+
+  scope :order_by_likes, -> { left_joins(:rates).where(rates: { status: 'like' }).group(:id).order('count(rates.id) desc') }
+  scope :order_by_created, -> { order(created_at: :desc) }
 end
